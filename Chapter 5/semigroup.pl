96b81c0be8b376c78838bc11cb7b6f74d1aca32f
dawn-ds-15 <haidang010505@gmail.com>
2025-10-13 08:23:32 +0700

    Update semigroup.pl

--- Chapter 5/semigroup.pl
+++ Chapter 5/semigroup.pl
@@ -1,1 +1,12 @@
 % Create a solution for Exercise 5.1, page 88. More precisely, you will attempt to prove the equality of left- and right-neutral elements of semigroups with PROLOG and fail. Try to explain why?
+% associative law
+equal(mul(mul(X,Y),Z), mul(X,mul(Y,Z))).
+% left neutral element
+equal(mul(el, X), X).
+% right neutral element
+equal(mul(X, er), X).
+% goal: prove equal(el, er).
+Prolog thì thất bại, vì:
+Nó chỉ dùng đơn nhất hóa cú pháp (syntactic unification),
+Không có quy tắc thay thế tương đương hai chiều,
+Và sử dụng chiến lược procedural depth-first, không phải logic đầy đủ.
