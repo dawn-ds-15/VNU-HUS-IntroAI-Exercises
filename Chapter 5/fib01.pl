e1d992846db6f9f3af072a6fccc17a1dbc596e2a
dawn-ds-15 <haidang010505@gmail.com>
2025-10-13 08:18:16 +0700

    Update fib01.pl

--- Chapter 5/fib01.pl
+++ Chapter 5/fib01.pl
@@ -1,1 +1,16 @@
 % Create a solution for Exercise 5.8(c)
+% fib01.pl
+:- dynamic fib/2.  % cho phép assert động
+
+fib(0, 1).
+fib(1, 1).
+fib(N, R) :-
+    N > 1,
+    (   fib_cached(N, R) -> true         % nếu đã có, dùng lại
+    ;   N1 is N - 1, fib(N1, R1),
+        N2 is N - 2, fib(N2, R2),
+        R is R1 + R2,
+        asserta(fib_cached(N, R))        % lưu lại kết quả
+    ).
+
+:- dynamic fib_cached/2.
